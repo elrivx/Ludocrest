f58490d1a74c42cf928dfefef6f3253221191326
Rodrigo Rivillo Brito <rrivillobrito@gmail.com>
2020-07-07 12:30:27 -0400

    Al oprimir el botón Lazar, ejecuta animación. Comienzo intento de mostrar valor del dado.

--- Scripts/tirarBoton.gd
+++ Scripts/tirarBoton.gd
@@ -10,4 +10,22 @@ func valorDado():
 func _on_tirarBoton_pressed():
 	valorDado()
 	print(numero)
-	$Dado.play("dice")
+	
+	var d = get_parent().get_node("Dice/Dado")
+	
+	d.play("dice")
+	
+	#match numero:
+	#	1:
+	#		d.stop()
+			
+	#	2:
+	#		d.stop()
+	#	3:
+	#		d.stop()
+	#	4:
+	#		d.stop()
+	#	5:
+	#		d.stop()
+	#	6:
+	#		d.stop()
